3f5ea6c3610d800e6e06e4a431dbf094603ccb56
Alvaro Hernandez <alvaroricardo98@gmail.com>
2023-07-06 17:43:39 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 
+import { useState } from 'react'
 import './App.css'
-
+//import CounterState from './components/CounterState'
 function App() {
+  const [count,setCount] = useState(-1)
+  let scoreColor = "gray";
+  
+  if(count> 0) scoreColor = "green";
+  else if (count < 0) scoreColor = "red";
 
   return (
     <div className="container">
       <div className="row max-height align-items-center">
         <div className="col-10 mx-auto  text-center main-container p-5">
           <h1 className="text-uppercase">counter</h1>
-          <h2 id="counter">0</h2>
+          <h2 style={{color: scoreColor}} id="counter">{count}</h2>
           <div className="btn-container d-flex justify-content-around flex-wrap">
-            <button className="btn counterBtn prevBtn text-uppercase m-2">
+            <button onClick={() => setCount(count-1)} className="btn counterBtn prevBtn text-uppercase m-2">
               lower count
             </button>
-            <button className="btn counterBtn nextBtn text-uppercase m-2">
+            <button onClick={() => setCount(count+1)} className="btn counterBtn nextBtn text-uppercase m-2">
               add count
             </button>
           </div>
